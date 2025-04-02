8ed2146bc2f8f44531f3a1238a16085022f782da
ifahimkhan <khan.fahim95@hotmail.com>
2025-04-03 00:59:34 +0530

    GPS Location Tracking Via OpenStreetMap without needing the Google Maps API Key

--- app/src/main/java/com/fahim/mapapp/MapsActivity.java
+++ app/src/main/java/com/fahim/mapapp/MapsActivity.java
@@ -1,88 +1,97 @@
 package com.fahim.mapapp;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import com.fahim.mapapp.databinding.ActivityMapsBinding;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.osmdroid.config.Configuration;
+import org.osmdroid.library.BuildConfig;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-    private GoogleMap mMap;
+public class MapsActivity extends FragmentActivity {
+
+    private MapView mMap;
     private ActivityMapsBinding binding;
     private double latitude = -31, longitude = 151;
+    private MyLocationNewOverlay myLocationOverlay;
+    private boolean showCurrentLocationNextTime = true;
+
     private String selectedDeviceId;
 
     public static void getInstance(Context context, String selectedDeviceId) {
         context.startActivity(new Intent(context, MapsActivity.class).putExtra("selectedDeviceId", selectedDeviceId));
     }
 
-    MarkerOptions marker = new MarkerOptions();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().setUserAgentValue(BuildConfig.LIBRARY_PACKAGE_NAME);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         selectedDeviceId = getIntent().getStringExtra("selectedDeviceId");
         Log.e("TAG", "onCreate: " + selectedDeviceId);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMap = binding.map;
+        updateMap();
+        binding.currentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showCurrentLocationNextTime) {
+                    setupLocationOverlay();
+                    showCurrentLocationNextTime = false;
+                } else {
+                    updateMap();
+                    showCurrentLocationNextTime = true;
+                }
+            }
+        });
+
 
     }
 
     private void updateMap() {
-        LatLng latLng = new LatLng(latitude, longitude);
-        marker.position(latLng)
-                .title("Your Location");
-        mMap.clear();
-        mMap.addMarker(marker);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.e("TAG", ":onMapReady ");
-        mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        // Add a marker in Sydney and move the camera
-        LatLng currentLocation = new LatLng(latitude, longitude);
-        mMap.addMarker(marker.position(currentLocation).title("Current Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        GeoPoint geoPoint = new GeoPoint(latitude, longitude);
+        mMap.setMultiTouchControls(true);
+        mMap.getController().setZoom(15.0);
+        mMap.getController().setCenter(geoPoint);
+        addMarker(geoPoint, selectedDeviceId);
 
     }
+
+    private void addMarker(GeoPoint point, String title) {
+        Marker marker = new Marker(mMap);
+        marker.setPosition(point);
+        marker.setTitle(title);
+        mMap.getOverlays().clear();
+        mMap.getOverlays().add(marker);
+        mMap.invalidate(); // Refresh the map
+    }
+
+    private void setupLocationOverlay() {
+        myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mMap);
+        myLocationOverlay.enableMyLocation(); // Enable GPS location
+        myLocationOverlay.enableFollowLocation(); // Auto-follow user
+        myLocationOverlay.setDrawAccuracyEnabled(true); // Show accuracy circle
+        mMap.getOverlays().add(myLocationOverlay);
+    }
+
 
     @Override
     protected void onResume() {
